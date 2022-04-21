9bfdad2ddc6bc6434048d04ae825ee5e550d30d8
vathsala <vathsala99r@gmail.com>
2022-04-21 20:31:49 +0530

    Create Customer Added

---
+++ VSFBAdminApp/Pages/AddCustomer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using VSFBAdminApp.Services;
+using VSFBankModels.Models;
+
+namespace VSFBAdminApp.Pages
+{
+    public partial class AddCustomer
+    {
+        [Inject]
+        public ICustomerDataService customerDataservice { get; set; }
+        [Parameter]
+        public string CustomerId { get; set; }
+        public Customer customer { get; set; } = new Customer();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            //customer = (await customerDataservice.GetCustomerDetails(CustomerId));
+        }
+
+        protected async Task CreateCustomerData()
+        {
+            await customerDataservice.AddCustomer(customer);
+
+        }
+    }
+}
